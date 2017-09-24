ed9eefabf8d4f0dc05ce06d8aa7c3e5b2cfbb3de
Nicolas Marty <nicolas.marty@gmx.net>
2017-09-24 11:48:21 +0200

    added ambilight client

---
+++ client/AmbiLightClient/AmbiLightClient/AmbiLightClient.cpp
@@ -0,0 +1,33 @@
+// "AmbiLightClient.cpp": Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <windows.h>
+
+
+int main()
+{
+	FARPROC pGetPixel;
+	HINSTANCE _hGDI = LoadLibrary("gdi32.dll");
+	if (_hGDI)
+	{
+		pGetPixel = GetProcAddress(_hGDI, "GetPixel");
+		HDC _hdc = GetDC(NULL);
+		if (_hdc)
+		{
+			POINT _cursor;
+			GetCursorPos(&_cursor);
+			COLORREF _color = (*pGetPixel) (_hdc, _cursor.x, _cursor.y);
+			int _red = GetRValue(_color);
+			int _green = GetGValue(_color);
+			int _blue = GetBValue(_color);
+
+			printf("Red: 0x%02x\n", _red);
+			printf("Green: 0x%02x\n", _green);
+			printf("Blue: 0x%02x\n", _blue);
+		}
+		FreeLibrary(_hGDI);
+	}
+    return 0;
+}
+
